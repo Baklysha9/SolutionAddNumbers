73713d996f7168a2ba656a76ed8812bfb871d583
Богдан Дунайцев <Baklysha@outlook.com>
2022-02-07 08:49:24 +0300

    Приложение для сложения двух чисел разработано

---
+++ SolutionAddNumbers/WpfAppAddOfTwoNumbers/WpfAppAddOfTwoNumbers.cpp
@@ -0,0 +1,20 @@
+﻿Title = "Сложение двух чисел" Height = "350" Width = "525" Topmost = "True" >
+<Grid>
+<TextBlock x : Name = "TextBlockNumber1" HorizontalAlignment = "Left" Height = "23"
+	Margin = "61,32,0,0" TextWrapping = "Wrap" Text = "Число 1"
+	VerticalAlignment = "Top" Width = "54" / >
+	<TextBlock x : Name = "TextBlockNumber2" HorizontalAlignment = "Left"
+	Margin = "61,80,0,0" TextWrapping = "Wrap" Text = "Число 2"
+	VerticalAlignment = "Top" / >
+	<Button x : Name = "ButtonAdd" Content = "Сложить"
+	HorizontalAlignment = "Left" Margin = "61,129,0,0"
+	VerticalAlignment = "Top" Width = "75" / >
+	<TextBox x : Name = "TextBoxNumber1" HorizontalAlignment = "Left" Margin = "200,32,0,0"
+	TextWrapping = "Wrap" VerticalAlignment = "Top" Width = "107" / >
+	<TextBox x : Name = "TextBoxNumber2" HorizontalAlignment = "Left" Height = "23"
+	Margin = "200,79,0,0" TextWrapping = "Wrap"
+	VerticalAlignment = "Top" Width = "107" / >
+	<TextBox x : Name = "TextBoxResult" HorizontalAlignment = "Left" Height = "23"
+	Margin = "200,128,0,0" TextWrapping = "Wrap"
+	VerticalAlignment = "Top" Width = "107" / >
+	< / Grid>
